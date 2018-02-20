ff3e9b9cd5c8ea87c03a03697bc86a3fabe11f75
Hodogor <kop.lop2012@yandex.ru>
2018-02-20 13:07:54 +0300

    Добавил класс

--- src/socket_client.py
+++ src/socket_client.py
@@ -1,19 +1,24 @@
 import socket
 
+class Socket_client:
+    def __init__(self, address='', port=8888):
+        socket_ = self.make_tcp_socket(address, port)
+        self.recive_time(socket_)
 
-def make_tcp_socket(address: str = 'localhost',
-                    port: int = 8888) -> socket.socket:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((address, port))
-    return s
+    def make_tcp_socket(self, address: str = 'localhost',
+                        port: int = 8888) -> socket.socket:
+        """ Подключить клиент к серверу"""
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((address, port))
+        return s
+    
+    
+    def recive_time(self, socket_: socket.socket) -> None:
+        """ Получить время от сервера"""
+        payload = socket_.recv(1024)
+        socket_.close()
+        print('Received time is {}'.format(payload.decode('ascii')))
 
 
-def recive_time(socket_: socket.socket) -> None:
-    payload = socket_.recv(1024)
-    socket_.close()
-    print('Received time is {}'.format(payload.decode('ascii')))
-
-
-def process(address='', port=8888):
-    socket_ = make_tcp_socket(address, port)
-    recive_time(socket_)
+pr = Socket_client('localhost')
+input()
--- src/socket_server.py
+++ src/socket_server.py
@@ -2,48 +2,53 @@ import select
 import socket
 import time
 
-_is_sending_time = None
+class Process:
+    def __init__(self, address: str, port: int, blocking: int):
+        self._is_sending_time = True
+        socket_ = self.make_tcp_socket(address, port, blocking)
+        method = self.send_time if blocking == 1 else self.send_time_non_blocking
+        try:
+            method(socket_)
+        except KeyboardInterrupt:
+            _is_sending_time = False
+            socket_.close()
+            print('Correctly closing')
+
+    def make_tcp_socket(self, address: str, port: int, blocking: int) -> socket.socket:
+        """ Создание TCP соединения"""
+        socket_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Создает сокет TCP
+        socket_.setblocking(blocking) 
+        socket_.bind((address, port)) # Присваивает порт и адрес
+        socket_.listen(5) #обслуживает не более 5 запросов
+        print("Создан сокет")
+        return socket_
 
 
-def make_tcp_socket(address: str, port: int, blocking: int) -> socket.socket:
-    socket_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_.setblocking(blocking)
-    socket_.bind((address, port))
-    socket_.listen(5)
-    return socket_
+    def send_time(self, socket_: socket.socket) -> None:
+        """ Функция отправит время клиенту"""
+        while self._is_sending_time:
+            client, address = socket_.accept()
+            print('Connected client from {}'.format(address))
+            payload = time.ctime(time.time()) + '\n'
+            client.send(payload.encode('ascii'))
+            client.close()
 
 
-def send_time(socket_: socket.socket) -> None:
-    while _is_sending_time:
-        client, address = socket_.accept()
-        print('Connected client from {}'.format(address))
-        payload = time.ctime(time.time()) + '\n'
-        client.send(payload.encode('ascii'))
-        client.close()
+    def send_time_non_blocking(self, socket_: socket.socket) -> None:
+        """ отправка времени без блокировки"""
+        while self._is_sending_time:
+            is_ready = select.select((socket_,), tuple(), tuple(), 1)
+            if not is_ready[0]:
+                print('Not ready')
+                continue
+    
+            client, address = socket_.accept()
+            print('Connected client from {}'.format(address))
+            payload = time.ctime(time.time()) + '\n'
+            client.send(payload.encode('ascii'))
+            client.close()
 
 
-def send_time_non_blocking(socket_: socket.socket) -> None:
-    while _is_sending_time:
-        is_ready = select.select((socket_,), tuple(), tuple(), 1)
-        if not is_ready[0]:
-            print('Not ready')
-            continue
-
-        client, address = socket_.accept()
-        print('Connected client from {}'.format(address))
-        payload = time.ctime(time.time()) + '\n'
-        client.send(payload.encode('ascii'))
-        client.close()
-
-
-def process(address='', port=8888, blocking=1):
-    global _is_sending_time
-    _is_sending_time = True
-    socket_ = make_tcp_socket(address, port, blocking)
-    method = send_time if blocking == 1 else send_time_non_blocking
-    try:
-        method(socket_)
-    except KeyboardInterrupt:
-        _is_sending_time = False
-        socket_.close()
-        print('Correctly closing')
+        
+if __name__ == '__main__':
+    proc = Process('', 8888, 1)
